d3e181861af73ea106fdf56a7b9316035023fac3
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2022-05-19 22:49:19 +0100

    Working on getting dynamic id on product page

--- scandistore/src/components/ProductPage/ProductPage.jsx
+++ scandistore/src/components/ProductPage/ProductPage.jsx
@@ -3,61 +3,64 @@ import "./ProductPage.scss";
 import { useParams } from "react-router-dom";
 import { request, gql } from "graphql-request";
 
+const id = useParams.id;
+
+const GET_PRODUCT = gql`
+  {
+      product (id: "huarache-x-stussy-le" )  {
+      id
+       name
+      inStock
+      gallery
+      description
+      category
+      attributes {
+        id
+        name
+        type
+        items {
+          displayValue
+          value
+          id
+        }
+      }
+      prices {
+        currency {
+          label
+          symbol
+        }
+        amount
+      }
+      brand
+      }
+  }
+  `;
+
 export default class ProductPage extends Component {
   constructor(props) {
     super(props);
     this.state = { product: [] };
   }
 
-  render() {
-    const id = useParams.id;
-    console.log(this.props);
+  
 
-    const GET_PRODUCT = gql`
-{
-    product (id: ${id} )  {
-    id
-     name
-    inStock
-    gallery
-    description
-    category
-    attributes {
-      id
-      name
-      type
-      items {
-        displayValue
-        value
-        id
-      }
-    }
-    prices {
-      currency {
-        label
-        symbol
-      }
-      amount
-    }
-    brand
-    }
-}
-`;
-componentDidMount() {
-    request("http://localhost:4000", GET_CLOTHES).then((data) => {
-      console.log(data);
+  componentDidMount() {
+    request("http://localhost:4000", GET_PRODUCT).then((data) => {
+      console.log("Product Data: ", data);
       this.setState({ data: data.category.products });
       console.log("Products State: ", this.state.data);
       
 
-    }).then(() => {
-      let handleAddProducts = () => {
-        this.props.addCartItemAction(this.state.data)
-      }
-
-      handleAddProducts();
-    });
+    })
   }
+
+  render() {
+    
+    console.log(this.props);
+
+   
+
+
 
 
     return (<div product={this.props.product}>d</div>);
